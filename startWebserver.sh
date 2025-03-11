6c7b3e78e1bb2273945281a86416f9d48aa7bf48
Curtis Andersen <curtis.andersen@dynon.com>
2025-03-11 09:24:53 -0700

    Added needed modifications to work on linux

--- startWebserver.sh
+++ startWebserver.sh
@@ -5,5 +5,9 @@ echo "Starting the SQL Server..."
 /mnt/c/Users/curti/Documents/CodingStandards/Web_Play_Ground/SetMySqlState.sh 1
 
 echo "Starting PHP built-in web server..."
-
-php -S localhost:8000 -t /mnt/c/Users/curti/Documents/CodingStandards/Web_Play_Ground
+if [[ "$(pwd)" == */Web_Play_Ground ]]; then
+    WEB_ROOT=$(pwd)
+else
+    WEB_ROOT="/mnt/c/Users/curti/Documents/CodingStandards/Web_Play_Ground"
+fi
+php -S localhost:8000 -t $WEB_ROOT
